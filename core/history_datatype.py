5fc16b5ff88e6a439b193535ba3c2611e3636c32
Dogukan Yilmaz <dogukanylmz137@hotmail.com>
2026-07-28 15:14:02 +0300

    Refactor CompanyHistory class to use OrderedDict for visitor tracking and implement max_visitors limit

--- core/history_datatype.py
+++ core/history_datatype.py
@@ -1,17 +1,30 @@
 from collections import OrderedDict
 
 class CompanyHistory:
-    def __init__(self):
-        self.visitors = {}
+    def __init__(self, max_visitors=50000):
+        # LRU mantığı kurabilmek için OrderedDict kullanıyoruz
+        self.visitors = OrderedDict()
+        self.max_visitors = max_visitors
 
     def add_query(self, vid, query):
         if vid not in self.visitors:
             self.visitors[vid] = QueryHistoryMRU()
+        else:
+            # Mevcut ziyaretçi işlem yaparsa, aktifliğini (LRU) güncelle
+            self.visitors.move_to_end(vid, last=True)
 
-        return self.visitors[vid].add_query(query)
+        result = self.visitors[vid].add_query(query)
+        
+        # Kapasite sınırı aşıldıysa, en eski (ilk sıradaki) ziyaretçiyi bellekten temizle
+        if len(self.visitors) > self.max_visitors:
+            self.visitors.popitem(last=False)
+
+        return result
 
     def get_queries_by_vid(self, vid):
         if vid in self.visitors:
+            # Ziyaretçi bilgisi okunduğunda da onu aktif sayıp sona at
+            self.visitors.move_to_end(vid, last=True)
             return self.visitors[vid].get_query()
         return []
 
